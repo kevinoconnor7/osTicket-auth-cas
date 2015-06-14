a7a78a0d0b43e4f726eca7d2ce7ba07fbc91084a
Kevin O'Connor <kevin.oconnor7@gmail.com>
2015-06-13 20:13:50 -0400

    Bump version

--- auth-cas/plugin.php
+++ auth-cas/plugin.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'id' =>             'auth:cas', # notrans
-    'version' =>        '0.2',
+    'version' =>        '0.3',
     'name' =>           /* trans */ 'JASIG CAS Authentication',
     'author' =>         'Kevin O\'Connor',
     'description' =>    /* trans */ 'Provides a configurable authentication
